5196fd6b3731ddeb1dfc70ed8b94862f3e24b4b2
raulamit <amit.c.raul@gmail.com>
2014-05-20 04:03:11 -0700

    UI nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   
   resources :dashboard
   root 'home#index'
+  
+  #match ':controller(/:action(/:id))', :via => :get
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
