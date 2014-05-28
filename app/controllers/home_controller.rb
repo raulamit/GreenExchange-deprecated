15d8cdbfd99367632845244af3375b4dba543c58
raulamit <amit.c.raul@gmail.com>
2014-05-27 23:17:45 -0700

    from other app

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,26 @@
 class HomeController < ApplicationController
 
 	def index
+		@navbar="home"
 		if user_signed_in?
 			redirect_to :controller => 'dashboard', :action => 'index'
 		end
 	end
+	
+	def services
+		@navbar= "services"
+	end
+
+	def news
+		@navbar= "news"
+	end
+
+	def aboutUs
+		@navbar= "aboutUs"
+	end
+
+	def contactUs
+		@navbar= "contactUs"
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :dashboard
   root 'home#index'
   
-  #match ':controller(/:action(/:id))', :via => :get
+  match ':controller(/:action(/:id))', :via => :get
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
